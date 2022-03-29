8b05ffb63cbe7805844bdbe4085bfc4700055caf
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-03-29 23:20:16 +0530

    refactor 1.1

--- main.py
+++ main.py
@@ -1,44 +1,12 @@
-import requests
-from defusedxml.minidom import parseString
-# defusedxml removes vulnerabilities
 import pandas as pd
 import pyfiglet as pfg
 from termcolor import cprint
-from view import select_article 
+
+from view import select_article
+from news_parser import get_google_news_result
 
 p = pfg.figlet_format("News Accumulator",font = 'slant')
 cprint(p,'blue',attrs = ['bold'])
-
-def get_google_news_result(term, count):
-    # Parses the string obtained from get request as xml document object
-    obj = parseString(
-        requests.get('http://news.google.com/news?q=%s&output=rss' %
-                     term).text)
-    items = obj.getElementsByTagName('item')
-    # Storing the Titles , Agencies, Links and Info
-    titles = []
-    links = []
-    agencies = []
-    infos = []
-    for item in items[:count]:
-        title, agency , link = '', '',''
-        info = {}
-        for node in item.childNodes:
-            if node.nodeName == 'title':
-                title = node.childNodes[0].data
-            elif node.nodeName == 'link':
-                link = node.childNodes[0].data
-            elif node.nodeName == 'pubDate':
-                info["publication date"] = node.childNodes[0].data
-            elif node.nodeName == 'description':
-                info["description"] = node.childNodes[0].data
-            elif node.nodeName == 'source':
-                agency = node.childNodes[0].data
-        titles.append(title)
-        links.append(link)
-        infos.append(info)
-        agencies.append(agency)
-    return titles, agencies , links , infos
 
 def get_inp():
     titleName = input("Enter the news title keyword: ")

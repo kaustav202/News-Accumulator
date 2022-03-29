2b1ce5523841ba565da41df5d3ca46ed26fb4918
kaustav202 <89788120+kaustav202@users.noreply.github.com>
2022-03-29 23:26:54 +0530

    refactor 1.1

---
+++ news_parser.py
@@ -0,0 +1,34 @@
+import requests
+from defusedxml.minidom import parseString
+# defusedxml removes vulnerabilities
+
+def get_google_news_result(term, count):
+    # Parses the string obtained from get request as xml document object
+    obj = parseString(
+        requests.get('http://news.google.com/news?q=%s&output=rss' %
+                     term).text)
+    items = obj.getElementsByTagName('item')
+    # Storing the Titles , Agencies, Links and Info
+    titles = []
+    links = []
+    agencies = []
+    infos = []
+    for item in items[:count]:
+        title, agency , link = '', '',''
+        info = {}
+        for node in item.childNodes:
+            if node.nodeName == 'title':
+                title = node.childNodes[0].data
+            elif node.nodeName == 'link':
+                link = node.childNodes[0].data
+            elif node.nodeName == 'pubDate':
+                info["publication date"] = node.childNodes[0].data
+            elif node.nodeName == 'description':
+                info["description"] = node.childNodes[0].data
+            elif node.nodeName == 'source':
+                agency = node.childNodes[0].data
+        titles.append(title)
+        links.append(link)
+        infos.append(info)
+        agencies.append(agency)
+    return titles, agencies , links , infos
